39fc5fc349d55cf3c302c52c8ad465d858fc94d9
Himani7i <153392456+Himani7i@users.noreply.github.com>
2025-07-30 01:05:30 +0530

    Fix axios baseURL to use env variable

--- client/src/api.js
+++ client/src/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
+let baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+if (baseURL.endsWith('/')) baseURL = baseURL.slice(0, -1); 
+
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
-  
+  baseURL: `${baseURL}/api`, 
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
-   console.log("👉 Sending token:", token);
+  //  console.log("👉 Sending token:", token);
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
 });
